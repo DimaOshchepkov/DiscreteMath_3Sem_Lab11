319f2a5466cc18dba924d2929b28e679d78c2618
OshchepkobDmitiy <odo.2003@yandex.ru>
2022-12-26 13:10:10 +0300

    first commit

--- Lab11/Lab11.cpp
+++ Lab11/Lab11.cpp
@@ -2,10 +2,100 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <algorithm>
+#include <fstream>
+#include <functional>
+
+class Sup
+{
+public:
+	std::vector<int> shortest;
+	std::vector<bool> visited;
+	std::vector<int> parent;
+	
+	Sup(int graphSize)
+		: shortest(graphSize, INT32_MAX)
+		, visited(graphSize, false)
+		, parent(graphSize, -1) {}
+
+
+};
+
+void InputGraph(const std::string& path)
+{
+	std::cout << "Input count top\n";
+	int countTop = 0;
+	std::cin >> countTop;
+
+	std::cout << "Input count edge\n";
+	int countEdge = 0;
+	std::cin >> countEdge;
+
+	std::cout << "Input graph (top1 <-> top2 sizeEdge)\n";
+
+	std::ofstream fout(path);
+	fout << countTop << " " << countEdge << '\n';
+	int t1, t2;
+	for (int i = 0; i < countEdge; i++)
+	{
+		std::cin >> t1 >> t2;
+		fout << t1 << " " << t2 << '\n';
+	}
+
+	fout.close();
+}
+
+std::vector<std::list<std::pair<int, int>>> ReadGraph(const std::string& path)
+{
+	std::ifstream fin(path);
+
+	int countTop, countEdge;
+	fin >> countTop >> countEdge;
+	std::vector<std::list<std::pair<int, int>>> graph(countTop, std::list<std::pair<int, int>>());
+	std::vector<std::vector<int>> graphMatrix(countTop, std::vector<int>(countTop));
+	int t1, t2, size;
+	while (fin >> t1 >> t2 >> size)
+	{
+		graph[t1].push_back({ t2, size });
+		graph[t2].push_back({ t1, size });
+	}
+
+	fin.close();
+
+	return graph;
+}
+
+std::list<std::pair<int, int>>::iterator min_element_if(std::list<std::pair<int, int>>& list,
+		std::function<bool(std::pair<int, int>)> Pred = [](const std::pair<int, int>& x) {return true; })
+{
+	auto itMin = list.end();
+	int min = INT32_MAX;
+
+	for (auto it = list.begin(); it != list.end(); it++)
+	{
+		if (it->second < min && Pred(*it))
+		{
+			min = it->second;
+			itMin = it;
+		}
+	}
+	return itMin;
+}
+
+void Dijckstra(const std::vector<std::list<std::pair<int, int>>>& graph)
+{
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	const std::string path = "graph.txt";
+	InputGraph(path);
+
+	ReadGraph(path);
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
